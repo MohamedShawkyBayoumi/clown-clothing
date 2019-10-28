2b996cd85c2c04787e96e82b7ba2352d33ae6bf2
unknown <ms@tarbeeta.com>
2019-10-28 19:20:07 +0200

    finishing MenuItems

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./menuitem.scss";
 
-const MenuItem = ({title, subtitle}) => {
+const MenuItem = ({title, subtitle, imageUrl, size}) => {
     return (
-        <div className="menu-item">
+        <div className={`menu-item ${size}`}>
+            <div className="background-image" style={{
+                backgroundImage: `url('${imageUrl}')`
+            }} />
             <div className="content">
-                <h1 className="title">{title}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">{subtitle}</span>
             </div>
         </div>
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,32 +1,11 @@
 import React from "react";
-import MenuItem from "../../components/MenuItem/MenuItem";
 import "./homepage.scss";
+import Directory from "../../components/Directory/Directory";
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <MenuItem 
-                    title="HATS"
-                    subtitle="SHOP NOW"
-                />
-                <MenuItem 
-                    title="JACKETS"
-                    subtitle="SHOP NOW"
-                />
-                <MenuItem 
-                    title="SNEAKERS"
-                    subtitle="SHOP NOW"
-                />
-                <MenuItem 
-                    title="WOMENS"
-                    subtitle="SHOP NOW"
-                />
-                <MenuItem 
-                    title="MENS"
-                    subtitle="SHOP NOW"
-                />
-            </div>
+            <Directory />
         </div>
     );
 }
