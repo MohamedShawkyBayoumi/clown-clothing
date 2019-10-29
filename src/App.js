0ad194e062181e79f25e5753fe1ea5978c4e1948
unknown <ms@tarbeeta.com>
2019-10-29 13:23:06 +0200

    working on Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from "./containers/HomePage/HomePage";
 import ShopPage from "./containers/ShopPage/ShopPage";
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
